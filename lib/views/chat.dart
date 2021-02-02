f4ab53b4da44f0c9955c50ed0449beea6949cc11
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2021-02-02 18:17:37 +0530

    Update chat.dart

--- lib/views/chat.dart
+++ lib/views/chat.dart
@@ -36,164 +36,169 @@ class _ChattingPlaceState extends State<ChattingPlace> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0x11ffffff),
-      height: 500,
-      width: 100,
-      child: Column(
-        children: [
-          StreamBuilder(
-            stream: chatController.message(
-                firebaseId: roomLogicController.roomFireBaseId),
-            builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-              if (snapshot.hasData) {
-                List<M> check = [];
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Container(
+        color: Color(0x11ffffff),
+        height: 500,
+        width: double.infinity,
+        child: Column(
+          children: [
+            StreamBuilder(
+              stream: chatController.message(
+                  firebaseId: roomLogicController.roomFireBaseId),
+              builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  List<M> check = [];
 
-                snapshot.data.snapshot.value.forEach((key, value) {
-                  text = [];
-                  messages.add(
-                      {"userId": value["userId"], "message": value["message"]});
-                  check.add(M(
-                      id: DateTime.parse(value["messageId"]),
-                      mesage: value["message"],
-                      userId: value["userId"],
-                      username: value["username"]));
-                });
+                  snapshot.data.snapshot.value.forEach((key, value) {
+                    text = [];
+                    messages.add({
+                      "userId": value["userId"],
+                      "message": value["message"]
+                    });
+                    check.add(M(
+                        id: DateTime.parse(value["messageId"]),
+                        mesage: value["message"],
+                        userId: value["userId"],
+                        username: value["username"]));
+                  });
 
-                check.sort((a, b) => (a.id).compareTo(b.id));
+                  check.sort((a, b) => (a.id).compareTo(b.id));
 
-                if (count != 0)
-                  chatScroll.animateTo(
-                    chatScroll.position.maxScrollExtent,
-                    duration: Duration(milliseconds: 200),
-                    curve: Curves.bounceIn,
+                  if (count != 0)
+                    chatScroll.animateTo(
+                      chatScroll.position.maxScrollExtent,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.bounceIn,
+                    );
+
+                  count++;
+
+                  // if (text == []) {
+                  //   text = check;
+                  // } else {
+                  //   check.removeWhere((element) {
+                  //     for (int i = 0; i < text.length; i++) {
+                  //       if (element["messageId"] == text[i]["messageId"]) {
+                  //         return true;
+                  //       }
+                  //     }
+                  //     return false;
+                  //   });
+                  //   text = temp;
+                  // }
+
+                  return Container(
+                    height: 400,
+                    child: ListView.builder(
+                        controller: chatScroll,
+                        itemBuilder: (ctx, i) {
+                          return roomLogicController.userId == check[i].userId
+                              ? Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Spacer(),
+                                    Container(
+                                      width: 150,
+                                      padding: EdgeInsets.all(8),
+                                      child: Card(
+                                        elevation: 2,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              check[i].username ==
+                                                      roomLogicController
+                                                          .adminKaNaam.obs.value
+                                                  ? Text("Admin",
+                                                      style: TextStyle(
+                                                          fontSize: 20,
+                                                          color: Colors.cyan))
+                                                  : Text(check[i].username),
+                                              SizedBox(height: 5),
+                                              Text(check[i].mesage),
+                                              SizedBox(height: 5)
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : Row(
+                                  children: [
+                                    Container(
+                                      // decoration:
+                                      //     BoxDecoration(color: Colors.cyan),
+                                      width: 150,
+                                      padding: EdgeInsets.only(right: 20),
+                                      child: Card(
+                                        elevation: 1,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              check[i].username ==
+                                                      roomLogicController
+                                                          .adminKaNaam.obs.value
+                                                  ? Text("Admin")
+                                                  : Text(check[i].username,
+                                                      style: TextStyle(
+                                                          fontSize: 20,
+                                                          color: Colors.red)),
+                                              Text(
+                                                check[i].mesage,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Spacer()
+                                  ],
+                                );
+                        },
+                        itemCount: check.length),
                   );
-
-                count++;
-
-                // if (text == []) {
-                //   text = check;
-                // } else {
-                //   check.removeWhere((element) {
-                //     for (int i = 0; i < text.length; i++) {
-                //       if (element["messageId"] == text[i]["messageId"]) {
-                //         return true;
-                //       }
-                //     }
-                //     return false;
-                //   });
-                //   text = temp;
-                // }
-
-                return Container(
-                  height: 400,
-                  child: ListView.builder(
-                      controller: chatScroll,
-                      itemBuilder: (ctx, i) {
-                        return roomLogicController.userId == check[i].userId
-                            ? Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Spacer(),
-                                  Container(
-                                    width: 150,
-                                    padding: EdgeInsets.all(8),
-                                    child: Card(
-                                      elevation: 2,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                          children: [
-                                            check[i].username ==
-                                                    roomLogicController
-                                                        .adminKaNaam.obs.value
-                                                ? Text("Admin",
-                                                    style: TextStyle(
-                                                        fontSize: 20,
-                                                        color: Colors.cyan))
-                                                : Text(check[i].username),
-                                            SizedBox(height: 5),
-                                            Text(check[i].mesage),
-                                            SizedBox(height: 5)
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            : Row(
-                                children: [
-                                  Container(
-                                    // decoration:
-                                    //     BoxDecoration(color: Colors.cyan),
-                                    width: 150,
-                                    padding: EdgeInsets.only(right: 20),
-                                    child: Card(
-                                      elevation: 1,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            check[i].username ==
-                                                    roomLogicController
-                                                        .adminKaNaam.obs.value
-                                                ? Text("Admin")
-                                                : Text(check[i].username,
-                                                    style: TextStyle(
-                                                        fontSize: 20,
-                                                        color: Colors.red)),
-                                            Text(
-                                              check[i].mesage,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Spacer()
-                                ],
-                              );
-                      },
-                      itemCount: check.length),
-                );
-              } else {
-                return Text("Getting your Chat");
-              }
-            },
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: message,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20))),
+                } else {
+                  return Text("Getting your Chat");
+                }
+              },
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: message,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
                 ),
-              ),
-              SizedBox(width: 5),
-              RaisedButton(
-                onPressed: () {
-                  chatController.sendMessage(
-                      firebaseId: roomLogicController.roomFireBaseId,
-                      message: message.text,
-                      userId: roomLogicController.userId,
-                      username: roomLogicController.userName.obs.value);
-                  message.clear();
-                  messages = [];
-                },
-                child: Text("Send"),
-              )
-            ],
-          )
-        ],
+                SizedBox(width: 5),
+                RaisedButton(
+                  onPressed: () {
+                    chatController.sendMessage(
+                        firebaseId: roomLogicController.roomFireBaseId,
+                        message: message.text,
+                        userId: roomLogicController.userId,
+                        username: roomLogicController.userName.obs.value);
+                    message.clear();
+                    messages = [];
+                  },
+                  child: Text("Send"),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
