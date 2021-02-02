4a46d12fa92ee2956e43dd2df49fc042e3d719c3
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2021-02-02 17:53:57 +0530

    Update chat.dart

--- lib/views/chat.dart
+++ lib/views/chat.dart
@@ -182,6 +182,7 @@ class _ChattingPlaceState extends State<ChattingPlace> {
                       message: message.text,
                       userId: roomLogicController.userId,
                       username: roomLogicController.userName.obs.value);
+                  message.clear();
                   messages = [];
                 },
                 child: Text("Send"),
