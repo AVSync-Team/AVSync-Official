56999350ef95269cd321a756286f2ed0ef45c1db
Rishabh <rishabhmishra23599@gmail.com>
2021-02-08 12:32:34 +0530

    yeh le

--- lib/controllers/themeData.dart
+++ lib/controllers/themeData.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class CustomThemeData {
+class CustomThemeData extends GetxController {
   static const customBlack = Color(0xff292727);
+  
+
+  void changeThemeToBlack() {}
+
+  void changeThemeToBlue() {}
 }
