b727be014439cb9f3b877fb17979b43a49754b53
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-07-24 17:19:29 +0530

    app state

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:VideoSync/controllers/themeData.dart';
 import 'package:VideoSync/views/createRoom.dart';
+import 'package:VideoSync/views/video%20players/videoPlayer.dart';
 // import 'package:VideoSync/views/homePage.dart';
 // import 'package:VideoSync/views/videoPlayer.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,8 +15,40 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // final themeController = Get.put(CustomThemeData());
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.detached) {
+      print("appp is inactive aur in background aur dead");
+      if (roomLogicController.roomFireBaseId != null) {
+        roomLogicController.userLeaveRoom(
+            firebaseId: roomLogicController.roomFireBaseId,
+            userId: roomLogicController.userId,
+            adminId: roomLogicController.adminId.value);
+        Get.offAll(CreateRoomScreen());
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
